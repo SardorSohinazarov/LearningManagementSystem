525dc56a2c05bcd8d02539d66f2e67b4d387a5a8
Sardor <sardorstudent0618@gmail.com>
2024-04-09 02:48:45 +0500

    Repositories

---
+++ src/LMS.Infrastructure/Repositories/CourseRepositories/CourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.Abstractions.Repositories;
+using LMS.Domain.Entities;
+using LMS.Infrastructure.Repositories.Base;
+
+namespace LMS.Infrastructure.Repositories.CourseRepositories
+{
+    public class CourseRepository : BaseRepsitory<Course>, ICourseRepository
+    {
+        public CourseRepository(AppDbContext context)
+            : base(context) { }
+    }
+}
---
+++ src/LMS.Infrastructure/Repositories/SpecialityRepositories/SpecialityRepository.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.Abstractions.Repositories;
+using LMS.Domain.Entities;
+using LMS.Infrastructure.Repositories.Base;
+
+namespace LMS.Infrastructure.Repositories.SpecialityRepositories
+{
+    public class SpecialityRepository : BaseRepsitory<Speciality>, ISpecialityRepository
+    {
+        public SpecialityRepository(AppDbContext context)
+            : base(context) { }
+    }
+}
