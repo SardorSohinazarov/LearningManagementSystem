34d653e2a25cbaf9f16b22217d7a9b2894a7a58c
Sardor <sardorstudent0618@gmail.com>
2024-04-09 02:48:51 +0500

    DI

---
+++ src/LMS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using LMS.Application.Abstractions.Repositories;
+using LMS.Infrastructure.Repositories.CourseRepositories;
+using LMS.Infrastructure.Repositories.SpecialityRepositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(configuration.GetConnectionString("Default")));
+
+            //course
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ISpecialityRepository, SpecialityRepository>();
+
+            return services;
+        }
+    }
+}
