f038aa57550e1891612dbdf87a8024911bf1b105
Sardor <sardorstudent0618@gmail.com>
2024-04-09 02:45:28 +0500

    Auditable and Base

---
+++ src/LMS.Domain/Comman/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Domain.Comman
+{
+    public class Auditable : Base
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/LMS.Domain/Comman/Base.cs
@@ -0,0 +1,7 @@
+﻿namespace LMS.Domain.Comman
+{
+    public class Base
+    {
+        public long Id { get; set; }
+    }
+}
