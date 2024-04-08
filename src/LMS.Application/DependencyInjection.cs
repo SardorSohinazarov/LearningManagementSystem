6b35cf03187b1425deed0917811a5318cd565d5e
Sardor <sardorstudent0618@gmail.com>
2024-04-09 02:48:12 +0500

    DI

---
+++ src/LMS.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using LMS.Application.Services.CourseServices;
+using LMS.Application.Services.SpecialityServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseSevice>();
+            services.AddScoped<ISpecialityService, SpecialityService>();
+
+            return services;
+        }
+    }
+}
