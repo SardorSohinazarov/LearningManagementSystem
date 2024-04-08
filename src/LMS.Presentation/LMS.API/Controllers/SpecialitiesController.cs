142016ce13ece72cd41cee5a610be8e726aa8612
Sardor <sardorstudent0618@gmail.com>
2024-04-09 02:49:15 +0500

    Controller and Service Registrations

---
+++ src/LMS.Presentation/LMS.API/Controllers/SpecialitiesController.cs
@@ -0,0 +1,36 @@
+﻿using LMS.Application.DataTransferObjects.Specialities;
+using LMS.Application.Services.SpecialityServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialitiesController : ControllerBase
+    {
+        private readonly ISpecialityService _specialityService;
+
+        public SpecialitiesController(ISpecialityService specialityService)
+            => _specialityService = specialityService;
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(SpecialityCreationDTO specialityCreationDTO)
+            => Ok(await _specialityService.CreateSpecialityAsync(specialityCreationDTO));
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(long id)
+            => Ok(await _specialityService.GetSpecialityByIdAsync(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+            => Ok(_specialityService.GetAllSpecialitiesAsync().ToList());
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(long id, SpecialityModificationDTO specialityModificationDTO)
+            => Ok(await _specialityService.UpdateSpecialityAsync(id, specialityModificationDTO));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(long id)
+            => Ok(await _specialityService.DeleteSpecialityAsync(id));
+    }
+}
--- src/LMS.Presentation/LMS.API/Program.cs
+++ src/LMS.Presentation/LMS.API/Program.cs
@@ -1,15 +1,16 @@
+using LMS.Application;
+using LMS.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
